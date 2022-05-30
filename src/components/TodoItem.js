a84a2bf29aa08e004936cfb7528c405181c393a9
wandergithub <wandergonzalez0@hotmail.com>
2022-05-30 14:45:01 -0400

    Add form functionality and event handlers

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,16 @@
 import React from "react"
 
-class TodoItem extends React.Component {
-  render() {
-    return <li>{this.props.todo.title}</li>
-  }
+function TodoItem(props) {
+  return <li>
+      <input 
+      type="checkbox"  
+      checked={props.todo.completed} 
+      onChange={() => props.handleChangeProps(props.todo.id)} />
+      {props.todo.title} 
+      <button onClick={()  => props.deleteTodoProps(props.todo.id)}>
+        Delete
+      </button>
+  </li>
 }
 
 export default TodoItem
